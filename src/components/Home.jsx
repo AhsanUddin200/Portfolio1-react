6034eb3d8976c2c541ba9018cead5c313bafc357
Ahsan Nasir <ahsanuddin160@gmail.com>
2023-12-22 22:00:33 +0500

    modificarion

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import HeroImage from "../assets/HeroImage.png";
 import { MdOutlineArrowRightAlt } from "react-icons/md";
 import { Link } from 'react-scroll';
+import { TypeAnimation } from 'react-type-animation';
 
 
 const Home = () => {
@@ -10,7 +11,23 @@ const Home = () => {
 
         <div className='max-w-screen-lg max-auto flex flex-col items-center justify-center h-full px-20 md:flex-row'>
             <div className='flex flex-col justify-center h-full'>
-                <h3 className='text-4xl sm:text-6xl font-bold text-yellow-50'> I am front end developer</h3>
+            <TypeAnimation
+        speed={30}
+        repeat={0}
+        wrapper='span'
+        className='text-4xl sm:text-6xl font-bold text-yellow-50'
+        sequence={[
+          "I am front end developer",
+          3000,
+          "Creating seamless and user-centric web experiences!",
+          2000,
+          "I am front end developer",
+          3000,
+        
+        ]}
+      />
+              { /*  <h3 className='text-4xl sm:text-6xl font-bold text-yellow-50'> I am front end developer</h3>*/}
+                
                 {/* <h6 className='text-white text mt-2 shadow-md'>Crafting Dynamic Frontend Experiences: Design, Integration, & User Engagement</h6> */}
             
             <p className='text-gray-500 py-4 max-w-md font-bold'>As a frontend developer with a keen eye for user interface design, I excel in seamlessly integrating APIs to enhance the functionality of web applications. I specialize in creating dynamic and responsive user experiences by connecting frontend interfaces with backend services through robust API integrations. My skill set includes not only crafting visually appealing interfaces but also ensuring efficient communication between the frontend and backend systems, resulting in a cohesive and feature-rich user experience.
@@ -31,7 +48,7 @@ const Home = () => {
             <img
   src={HeroImage}
   alt="my image"
-  className='rounded-3xl mx-auto w-full md:w-1/2 my-2 md:my-1 min-w-40 duration-500 hover:scale-150'
+  className='rounded-3xl mx-auto w-full md:w-1/2 my-2 md:my-1 min-w-40 duration-500 hover:scale-150 shadow-2xl shadow-gray-700'
 />
 </div>
 
